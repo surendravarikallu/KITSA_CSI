11379aa17b656cb88c9ded6b92a18910a2fb5ca3
surendravarikallu <varikallusurendra@gmail.com>
2026-02-27 10:25:53 +0530

    Add Vercel serverless configuration

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let isReady = false;
+
+// Initialize routes asynchronously but map the requests to it
+const init = async () => {
+    if (!isReady) {
+        // We pass a dummy server just to satisfy the function signature
+        // Since Vercel Serverless handles the HTTP server part
+        const mockServer: any = {
+            listen: () => { },
+            on: () => { },
+            close: () => { },
+        };
+        await registerRoutes(mockServer, app);
+        isReady = true;
+    }
+};
+
+export default async (req: any, res: any) => {
+    await init();
+    return app(req, res);
+};
